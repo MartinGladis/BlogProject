fa34235d89549b4b3ab2add1c912a5359d1b232c
Martin Gladis <martin.gladis@axxiomehealth.com>
2022-06-13 13:47:23 +0200

    add migrations

---
+++ migrations/m220613_100356_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m220613_100356_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(255)->notNull(),
+            'name' => $this->string(255)->notNull(),
+            'surname' => $this->string(255)->notNull(),
+            'birthdate' => $this->date()->notNull(),
+            'street_name' => $this->string(255),
+            'number' => $this->string(10),
+            'postcode' => $this->string(6),
+            'email' => $this->string(255)->notNull(),
+            'pesel' => $this->string(11),
+            'registered_at' => $this->timestamp()->notNull(),
+            'last_login' => $this->dateTime()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
---
+++ migrations/m220613_102708_create_post_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%post}}`.
+ */
+class m220613_102708_create_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+            'topic' => $this->string(255)->notNull(),
+            'description' => $this->text()->notNull(),
+            'user_id' => $this->integer(),
+            'attachment' => $this->string(255),
+            'create_at' => $this->timestamp()->notNull(),
+            'update_at' => $this->datetime()
+        ]);
+
+        $this->addForeignKey(
+            'FK_post_user',
+            'tbl_post',
+            'user_id',
+            'tbl_user',
+            'id'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%post}}');
+    }
+}
